23331c59faf7a439a606843cdcfd7009c51fb119
Craciun Catalin Ionut <craciun.catalin92x@gmail.com>
2023-03-01 10:20:33 +0200

    seminar02

--- Seminar02/src/src/cts/s02/principii_clean_code/main/Program.java
+++ Seminar02/src/src/cts/s02/principii_clean_code/main/Program.java
@@ -1,23 +1,24 @@
 package src.cts.s02.principii_clean_code.main;
 
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
 import java.util.List;
 
 import src.cts.s02.principii_clean_code.clase.Angajat;
-import src.cts.s02.principii_clean_code.clase.Utils;
+import src.cts.s02.principii_clean_code.clase.Aplicant;
+import src.cts.s02.principii_clean_code.clase.readers.AngajatReader;
+import src.cts.s02.principii_clean_code.clase.readers.AplicantReader;
 
 public class Program {
-
-	public static void main(String[] args) {
-		List<Angajat> listaAngajati;
-		try {
-			listaAngajati = Utils.readAngajati("angajati.txt");
-			for(Angajat angajat:listaAngajati)
-				System.out.println(angajat.toString());
-		} catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-	}
-
+    public static void main(String[] args) {
+        List<Aplicant> listaAngajati;
+        try {
+            AplicantReader aplicantReader = new AngajatReader();
+            listaAngajati = aplicantReader.readAplicants("angajati.txt");
+            for (Aplicant aplicant : listaAngajati)
+                System.out.println(aplicant.toString());
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
 }
